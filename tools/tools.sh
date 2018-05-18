36e01cd6c9a7d3796ad9b3ab6cf875d6a27019fb
Ameer Abbas <ameer00@gmail.com>
2018-05-18 16:06:52 -0700

    Update tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -20,3 +20,10 @@ wget https://github.com/kubernetes/kops/releases/download/1.8.0/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 ~/bin/kops
 PATH=$PATH:$HOME/.local/bin/
+
+# Install kube ps1
+git clone https://github.com/jonmosco/kube-ps1.git
+echo 'source $HOME/kube-ps1/kube-ps1.sh' >> ~/.bashrc
+export VAR="PS1='[\W $(kube_ps1)]\$ '"
+echo $VAR >> ~/.bashrc
+source $HOME/.bashrc
